09646ba1e3ff7a898d6290a8628d1034a099ae03
dayeon <sdy32@naver.com>
2023-01-25 05:33:50 +0900

    프로젝트 기초 작업

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,22 @@
+import { useNavigate, useSearchParams } from "react-router-dom"
+
 const Edit = () => {
+  const navigate = useNavigate()
+  const [searchParams, setSearchParams] = useSearchParams()
+
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지입니다.</p>
+
+      <button onClick={() => {
+        navigate("/")
+      }}>HOME으로 가기</button>
+
+      <button onClick={() => {
+        navigate(-1)
+      }}>뒤로가기</button>
     </div>
   )
 }

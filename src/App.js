63bbdf2389de68ac36f4fb07ba6cd0cf4920207a
Da Yeon Sung <sdy32@naver.com>
2023-01-25 15:11:49 +0900

    Main 달력 기능 구현

--- src/App.js
+++ src/App.js
@@ -38,11 +38,45 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext()
 export const DiaryDispatchContext = React.createContext()
 
+const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: "오늘의 일기 1번",
+    date: 1674607088549,
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "오늘의 일기 2번",
+    date: 1674607088550,
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "오늘의 일기 3번",
+    date: 1674607088551,
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "오늘의 일기 4번",
+    date: 1674607088552,
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "오늘의 일기 5번",
+    date: 1674607088553,
+  },
+]
+
 function App() {
 
   const [data, dispatch] = useReducer(reducer, [])
 
   const dataId = useRef(0)
+
   // CREATE
   const onCreate = (date, content, emotion) => {
     dispatch({
